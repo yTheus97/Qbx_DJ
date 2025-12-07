9b0396e48f3398ad18870b553d32f96dc9182510
yTheus97 <matheusbm202019@gmail.com>
2025-12-07 02:14:48 -0300

    Refactor fxmanifest.lua for better structure

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,34 @@
 fx_version 'cerulean'
-game 'gta5'
-
+games { 'gta5' }
+lua54 'yes'
 author 'Antigravity'
 description 'Immersive DJ Job Script'
 version '0.1.0'
 
+
+dependencies {
+    'ox_lib', 
+    'qbx_core'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua', 
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
+
 ui_page 'html/index.html'
 
 files {
+    'locales/*.json',
     'html/index.html',
     'html/style.css',
-    'html/script.js'
+    'html/script.js',
 }
-
-client_script 'client.lua'
-server_script 'server.lua'
